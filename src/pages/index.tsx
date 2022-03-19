91dec2358620095553a620aac32e868c32e482bd
Emily Sturman <emily@sturman.org>
2022-03-19 00:22:12 -0500

    Getting started

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,82 +1,11 @@
-import * as React from "react"
-
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
+import * as React from "react";
+import "../stylesheets/index.scss";
 
 const docLink = {
   text: "TypeScript Documentation",
   url: "https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative" as "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
+  color: "#8954A8"
+};
 
 // data
 const links = [
@@ -128,42 +57,42 @@ const links = [
 // markup
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
+    <main className={"pageStyles"}>
       <title>Home Page</title>
-      <h1 style={headingStyles}>
+      <h1 className={"headingStyles"}>
         Congratulations
         <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
+        <span className={"headingAccentStyles"}>— you just made a Gatsby site! </span>
         🎉🎉🎉
       </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this page
+      <p className={"paragraphStyles"}>
+        Edit <code className={"codeStyles"}>src/pages/index.tsx</code> to see this page
         update in real-time. 😎
       </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
+      <ul className={"listStyles"}>
+        <li className={"docLinkStyle"}>
           <a
-            style={linkStyle}
+            className={"linkStyle"}
             href={`${docLink.url}?utm_source=starter&utm_medium=ts-docs&utm_campaign=minimal-starter-ts`}
           >
             {docLink.text}
           </a>
         </li>
         {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
+          <li key={link.url} className={"listItemStyles"} style={{ color: link.color }}>
             <span>
               <a
-                style={linkStyle}
+                className={"linkStyle"}
                 href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter-ts`}
               >
                 {link.text}
               </a>
               {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
+                <span className={"badgeStyle"} aria-label="New Badge">
                   NEW!
                 </span>
               )}
-              <p style={descriptionStyle}>{link.description}</p>
+              <p className={"descriptionStyle"}>{link.description}</p>
             </span>
           </li>
         ))}
