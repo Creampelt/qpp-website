79b7248cab65b13271c4b30ead24b74a9a8d9959
Emily Sturman <emily@sturman.org>
2022-07-25 08:44:49 -0700

    Create Netlify function to send email from Get Involved form response
    - Add functions directory to project
    - Add SendGrid API

---
+++ functions/submit-get-involved-form/submit-get-involved-form.ts
@@ -0,0 +1,47 @@
+import { Handler } from "@netlify/functions";
+import sgMail from "@sendgrid/mail";
+
+export const handler: Handler = async (event) => {
+  const params = JSON.parse(event.body || "{}");
+  if (Object.keys(params).length === 0) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: "Please provide parameters." })
+    };
+  }
+
+  const { SENDGRID_API_KEY, AUTHORIZED_SENDER, TEMPLATE_ID } = process.env;
+  if (!AUTHORIZED_SENDER) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Something went wrong. Please try again later." })
+    };
+  } else if (!SENDGRID_API_KEY || !TEMPLATE_ID) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: `Could not send email. Please reach out to ${AUTHORIZED_SENDER}.` })
+    };
+  }
+
+  sgMail.setApiKey(SENDGRID_API_KEY);
+  const msg = {
+    to: AUTHORIZED_SENDER,
+    from: AUTHORIZED_SENDER,
+    templateId: TEMPLATE_ID,
+    dynamicTemplateData: params
+  };
+
+  try {
+    await sgMail.send(msg);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Email sent successfully." })
+    };
+  } catch (e) {
+    const error = e as Error;
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: error.message })
+    };
+  }
+}
