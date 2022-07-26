6f23f34082abe787f6fc1806ccf2febf60d32f58
Emily Sturman <emily@sturman.org>
2022-07-26 16:17:21 -0700

    Adapt upcoming events styling for mobile

--- src/page-sections/get-involved.tsx
+++ src/page-sections/get-involved.tsx
@@ -17,9 +17,9 @@ const EventElement: React.FunctionComponent<UpcomingEvent> = ({ name, location, 
   <div className={"event"} key={`${name}_${location}_${start.format(DATE_FORMAT)}_${end.format(DATE_FORMAT)}`}>
     <div className={"bullet"} />
     <div>
-      <h6>{name}</h6>
-      <p>{location}</p>
-      <p>
+      <h6 className={"column name-column"}>{name}</h6>
+      <p className={"column"}>{location}</p>
+      <p className={"column"}>
         {start.isSame(end, "day")
           ? `${start.format("M/D")} from ${start.format("h:mma")} to ${end.format("h:mma")}`
           : `${start.format("M/D [at] h:mma")} to ${end.format("M/D [at] h:mma")}`
